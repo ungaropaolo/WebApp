4b7665db43868361bcb317a5542b7e7d9ce8c873
ungaropaolo <paolo.ungaro@atos.net>
2023-06-12 15:03:30 +0200

    Creazione di una interfaccia

--- firstapp/src/app/home/home.component.ts
+++ firstapp/src/app/home/home.component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { User } from '../user';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './home.component.html',
-  template: `
-    <p>
-      home works!
-    </p>
-  `,
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 
+  user: User = {
+    name : "nome",
+    surname : ""
+  }
 }
